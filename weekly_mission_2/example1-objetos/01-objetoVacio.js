6b70624f65615893c861e77c0cc58563308e39ff
Angel <angelSistemas21@gmail.com>
2022-04-18 07:32:01 -0500

    add: Weekly mission 2 - 01 Ejemplo de objeto vacío

---
+++ weekly_mission_2/example1-objetos/01-objetoVacio.js
@@ -0,0 +1,5 @@
+console.log("Creación de un objeto vacío")
+const myFirstObject = {} //empty object
+console.log(myFirstObject)
+// Interpolar variables
+console.log(`1. Objeto vacío: ${myFirstObject}`)
