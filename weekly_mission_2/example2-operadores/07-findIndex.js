58fc975bf1976177e5c6750735278ab2e4674e9a
Angel <angelSistemas21@gmail.com>
2022-04-19 17:41:14 -0500

    update: Weekly mission 2 - Example 2 Operadores: 07-FindIndex. Ejercicio 2

--- weekly_mission_2/example2-operadores/07-findIndex.js
+++ weekly_mission_2/example2-operadores/07-findIndex.js
@@ -2,3 +2,15 @@
 const names13 = ['ExplorerAngel 1', 'Explorer 2', 'Explorerr 3']
 const result = names13.findIndex((name) => name.length > 7)
 console.log("Ejemplo 13: Primer elemento cuya palabra sea mayor a 7 esta en la posición "+ result)
+
+
+
+const array1 = [5, 12, 8, 130, 44];
+// Forma 1
+const isLargeNumber2 = (element) => element > 13;
+console.log(array1.findIndex(isLargeNumber2));
+
+// Forma 2
+const isLargeNumber = array1.findIndex((num) => num > 13);
+console.log("Ejemplo de prueba, en donde se regresa la primer posición que encuentre que sea mayor a 13 que en este caso sería la posición 3: " + isLargeNumber);
+// expected output: 3
