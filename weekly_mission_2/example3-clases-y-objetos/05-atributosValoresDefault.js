f1f5258d09f83e3a68cb812a04f5c5ff73061703
Angel <angelSistemas21@gmail.com>
2022-04-21 01:59:07 -0500

    update: Weekly mission 2 - Example 3 Clases y objetos: 05-Atributos con valores por default

--- weekly_mission_2/example3-clases-y-objetos/05-atributosValoresDefault.js
+++ weekly_mission_2/example3-clases-y-objetos/05-atributosValoresDefault.js
@@ -1,17 +1,29 @@
-// Ejemplo 4: Métodos en los objetos
-class Repository {
-    constructor(name, author, language, stars, saludos) {
-        this.name = name
-        this.author = author
-        this.language = language
-        this.stars = stars
-        this.saludos = saludos
+// Ejemplo 5: Atributos con valores por default
+class PullRequest {
+    constructor(repo, title, lines_changed) {
+        this.repo = repo
+        this.title = title
+        this.lines_changed = lines_changed
+        this.status = "OPEN"
+        this.dateCreated = new Date() // esto guardará la fecha actual en que se instancia el objeto
     }
 
-    getInfo() { // es una función que ejecutará cualquier objeto instanciado de esta clase
-        return `Repository ${this.name} has ${this.stars} stars and the author is ${this.author}. ${this.saludos} `
+    getInfo() {
+        return `This PR is in the repo: ${this.repo} (status: ${this.status}) and was created on ${this.dateCreated}`
+    }
+    getInfo2() {
+        return `This PR is in the repo: ${this.title} (status: ${this.status}) and was created on ${this.dateCreated}`
     }
 }
-console.log("Ejemplo 4: Métodos en los objetos")
-const myRepo = new Repository("LaunchX", "angel", "js", 100, "Saludos")
-console.log(myRepo.getInfo())
+
+console.log("Ejemplo 5: Atributos con valores por default")
+const myPR1 = new PullRequest("LaunchX", "Mi Primer PR", 100)
+console.log(myPR1.getInfo())
+
+// Puedes instanciar n cantidad de objetos de la misma clase
+const myPR2 = new PullRequest("Launch", "Mi segundo PR", 99)
+console.log(myPR2.getInfo())
+
+// Puedes instanciar n cantidad de objetos de la misma clase
+const myPR3 = new PullRequest("Launch X - Innovaccion", "Mi segundo PR", 99)
+console.log(myPR2.getInfo2())
