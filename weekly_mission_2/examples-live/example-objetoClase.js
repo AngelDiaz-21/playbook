b6c1270ebd12dbb9362fa9e923c7260bddc8f5ea
Angel <angelSistemas21@gmail.com>
2022-04-25 18:02:58 -0500

    add: Ejemplo del live 3 semana 2 - Objeto y clase vacía: Agregando constructor a la clase Ajolonauta

--- weekly_mission_2/examples-live/example-objetoClase.js
+++ weekly_mission_2/examples-live/example-objetoClase.js
@@ -1,11 +1,37 @@
 console.log("POO")
 
 // * Objeto de js
-const woopa = {
+const woopa_objeto = {
     name: "woopa",
     mission: "Node JS",
     age: 12,
     color: "Pink"
 }
 
-// * Clase 
+// * Clase
+// Woopa es un ajolonauta, así que haremos una clase que creee varios ajolanautas
+// Un objeto tiene 2 componentes (atributos y métodos)
+class Ajolonauta {
+    // Los constructores nos ayudan a construir los objetos que se instancian de esta clase. En el constructor va a tomar esos parametros que se los estamos enviando de algun lado
+    constructor(name, mission, age, color){
+        this.name = name
+        this.mission = mission
+        this.age = age
+        this.color = color
+    }
+
+}
+
+// * Haremos un objeto de clase
+// * Se instancia un objeto
+const wooopa = new Ajolonauta("Woopa", "Node JS", 18, "Pink")
+console.log(wooopa)
+
+// A partir de la definicion de una clase se pueden instancias más objetos. SI utilizamos la analogía del molde de galletas, el molde seria la clase y las galletas del molde serían las instancias de los objetos
+// Estamons instanciando 3 objetos diferentes de la misma clase
+const wooper = new Ajolonauta("Wooper", "PHP", 23, "Blue")
+console.log(wooper)
+const woopy = new Ajolonauta()
+
+
+// ! Has 2 tipos de objetos. El 1° es un objeto creado de una clase y los objetos de JavaScript
